f2f16a2d31baeedd7b4bc86379746021d0fc2899
robertmwenja4 <robertmwenja4@gmail.com>
2022-04-27 22:05:24 +0300

    GSM completed

--- GetRFID_ID_FROM_ARDUINO/GetRFID_ID_FROM_ARDUINO.ino
+++ GetRFID_ID_FROM_ARDUINO/GetRFID_ID_FROM_ARDUINO.ino
@@ -5,11 +5,13 @@
 
 #define SS_PIN 10
 #define RST_PIN 9
-#define LED_G 5 //define green LED pin
-#define LED_R 4 //define red LED
 #define ACCESS_DELAY 2000
 #define DENIED_DELAY 1000
+SoftwareSerial sim800L(7, 6);
 SoftwareSerial ArduinoUno(3, 2);
+SoftwareSerial espSerial(5, 4);
+int data;
+const String num = "+254719189576";
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 
 void setup()
@@ -17,15 +19,24 @@ void setup()
   Serial.begin(9600);   // Initiate a serial communication
   SPI.begin();          // Initiate  SPI bus
   ArduinoUno.begin(4800);
+  espSerial.begin(4800);
   mfrc522.PCD_Init();   // Initiate MFRC522
-  pinMode(LED_G, OUTPUT);
-  pinMode(LED_R, OUTPUT);
+  sim800L.begin(9600);
+  Serial.println("Initializing...");
+  //Once the handshake test is successful, it will back to OK
+  sim800L.println("AT");
+  delay(1000);
+  sim800L.println("AT+CMGF=1");
+  delay(1000);
   Serial.println("Put your card to the reader...");
   Serial.println();
 
 }
 void loop()
 {
+
+  updateSerial();
+  // Look for new ca
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent())
   {
@@ -37,7 +48,7 @@ void loop()
     return;
   }
   //Show UID on serial monitor
-  Serial.print("UID tag :");
+  Serial.print("Tagging UID tag :");
   String content = "";
   byte letter;
   for (byte i = 0; i < mfrc522.uid.size; i++)
@@ -52,12 +63,54 @@ void loop()
   content.toUpperCase();
   // Create JSON Object to pass the string to node MCU using content.substring(1)
   String val = content.substring(1);
-  //  ArduinoUno.println(content.substring(1));
-  //  ArduinoUno.print('\n');
+
   StaticJsonDocument<1000> doc;
   doc["cardID"] = val;
   //Send data to NodeMCU
   serializeJson(doc, ArduinoUno);
   delay(500);
+}
+void updateSerial()
+{
+  delay(500);
+  while (Serial.available())
+  {
+    sim800L.write(Serial.read());//Forward what Serial received to Software Serial Port
+  }
+  while (sim800L.available())
+  {
+    Serial.write(sim800L.read());//Forward what Software Serial received to Serial Port
+  }
+}
+void receiveNo() {
+  while (espSerial.available() > 0) {
+    StaticJsonDocument<1000> doc;
+    DeserializationError error = deserializeJson(doc, espSerial);
 
+    // Test parsing
+    while (error) {
+      Serial.println("Invalid JSON Object");
+      delay(500);
+      DeserializationError error = deserializeJson(doc, espSerial);
+      return;
+    }
+    String card = doc["Number"];
+    Serial.print("Number received is: ");
+    Serial.println(card);
+
+    delay(5000);
+  }
+}
+void send_sms()
+{
+  Serial.println("sending sms....");
+  delay(50);
+  sim800L.print("AT+CMGF=1\r");
+  delay(1000);
+  sim800L.print("AT+CMGS=\"" + num + "\"\r");
+  delay(1000);
+  sim800L.print("Your Bag is in CheckIn 2");
+  delay(100);
+  sim800L.write(0x1A); //ascii code for ctrl-26 //Serial2.println((char)26); //ascii code for ctrl-26
+  delay(5000);
 }
--- NodeMCU/NodeMCU.ino
+++ NodeMCU/NodeMCU.ino
@@ -4,32 +4,32 @@
 #include <SPI.h>
 #include <MFRC522.h>
 #include <ESP8266HTTPClient.h>
-
+#include <Wire.h>
 #define SS_PIN D4
-#define RST_PIN D1
+#define RST_PIN D0
 
 
 const char *ssid = "Robaranks"; //WIFI NAME OR HOTSPOT
 const char *password = "1234567890"; //WIFI PASSWORD POR MOBILE HOTSPOT PASSWORD
-const char* host = "http://4eec-197-156-190-154.ngrok.io"; //LOCAL IPv4 ADDRESS...ON CMD WINDOW TYPE ipconfig/all
+const char* host = "http://334f-154-122-213-31.ngrok.io"; //LOCAL IPv4 ADDRESS...ON CMD WINDOW TYPE ipconfig/all
 const uint16_t port = 80; //PORT OF THE LOCAL SERVER
-int rank = 4; //THE RANK WHOSE DATA YOU WANT TO FETCH
 HTTPClient http;
 
-//GSM Module RX pin to NodeMCU D3
-//GSM Module TX pin to NodeMCU D4
-#define rxPin D0
-#define txPin D8
-SoftwareSerial sim800L(rxPin, txPin);
-SoftwareSerial uno(D2, D3);
+//GSM Module RX pin to NodeMCU D0
+//GSM Module TX pin to NodeMCU D8
+//#define rxPin D2
+//#define txPin D3
+SoftwareSerial sim800L(D2, D3);
+SoftwareSerial uno(D1, D8);
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 void setup() {
   Serial.begin(9600);
   uno.begin(4800);
-  pinMode(D2, INPUT);
-  pinMode(D3, OUTPUT);
+
+  //  pinMode(D0, INPUT);
+  //  pinMode(D8, OUTPUT);
   WiFi.mode(WIFI_OFF);
   delay(1000);
   WiFi.mode(WIFI_STA);
@@ -50,14 +50,14 @@ void setup() {
 
   SPI.begin();
   mfrc522.PCD_Init();
-
+  //espS.begin(4800);
   sim800L.begin(9600);
   Serial.println("Initializing...");
   //Once the handshake test is successful, it will back to OK
   sim800L.println("AT");
-  delay(1000);
+  updateSerial();
   sim800L.println("AT+CMGF=1");
-  delay(1000);
+  updateSerial();
 
 }
 void SendCardID(String cardID) {
@@ -82,7 +82,7 @@ void SendCardID(String cardID) {
     serializeJson(doc, json);
     Serial.println(json);
     //Replace the ngrok url here
-    http.begin(client, "http://4eec-197-156-190-154.ngrok.io/api/postdata");     //Specify request destination
+    http.begin(client, "http://334f-154-122-213-31.ngrok.io/api/postdata");     //Specify request destination
     http.addHeader("Content-Type", "application/json");
     int httpCode = http.POST(json);   //Send the request
     String payload = http.getString();                                        //Get the response payload
@@ -122,25 +122,30 @@ void GetData(String cardID) {
     doc["phone"] = phone;
 
     String card = cardID;
-    
+
     serializeJson(doc, json);
     Serial.println(json);
     //Replace the ngrok url here
-    http.begin(client, "http://4eec-197-156-190-154.ngrok.io/api/product/search/" + String(cardID));   //Specify request destination
+    http.begin(client, "http://334f-154-122-213-31.ngrok.io/api/product/search/" + String(cardID));   //Specify request destination
     http.addHeader("Content-Type", "application/json");
     int httpCode = http.GET();   //Send the request
     String payload = http.getString();                                        //Get the response payload
 
     Serial.println(httpCode);   //Print HTTP  code
     Serial.println(payload);    //Print request response payload
-    if (payload != "") {
 
+    if (payload == "Creating Bag........") {
+      return;
+    }
+    else {
       String Number = payload.substring(1, 10);
       String no = "+254" + Number;
       Serial.println(no);
+
       send_sms(no);
+      delay(2000);
       String json;
-      String dest = "CheckIn 2";
+      String dest = "CheckIn 1";
       //Location stored in JSON format
       DynamicJsonDocument doc(200);
       doc["bag_tagID"] = cardID;
@@ -148,7 +153,7 @@ void GetData(String cardID) {
       serializeJson(doc, json);
       Serial.println(json);
       //Replace the ngrok url here
-      http.begin(client, "http://4eec-197-156-190-154.ngrok.io/api/status");     //Specify request destination
+      http.begin(client, "http://334f-154-122-213-31.ngrok.io/api/status");     //Specify request destination
       http.addHeader("Content-Type", "application/json");
       int httpCode = http.POST(json);   //Send the request
       String payload = http.getString();                                        //Get the response payload
@@ -186,14 +191,22 @@ void cardIDfromUno(String cardID) {
     serializeJson(doc, json);
     Serial.println(json);
     //Replace the ngrok url here
-    http.begin(client, "http://4eec-197-156-190-154.ngrok.io/api/product/search/" + String(cardID));   //Specify request destination
+    http.begin(client, "http://334f-154-122-213-31.ngrok.io/api/product/search/" + String(cardID));   //Specify request destination
     http.addHeader("Content-Type", "application/json");
     int httpCode = http.GET();   //Send the request
     String payload = http.getString();                                        //Get the response payload
 
     Serial.println(httpCode);   //Print HTTP  code
     Serial.println(payload);    //Print request response payload
-    if (payload == "success") {
+    if (payload == "Creating Bag........") {
+      return;
+    }
+    else {
+      String Number = payload.substring(1, 10);
+      String no = "+254" + Number;
+      Serial.println(no);
+      send_sms1(no);
+      delay(2000);
       String json;
       String dest = "CheckIn 2";
       //Location stored in JSON format
@@ -203,7 +216,7 @@ void cardIDfromUno(String cardID) {
       serializeJson(doc, json);
       Serial.println(json);
       //Replace the ngrok url here
-      http.begin(client, "http://4eec-197-156-190-154.ngrok.io/api/status");     //Specify request destination
+      http.begin(client, "http://334f-154-122-213-31.ngrok.io/api/status");     //Specify request destination
       http.addHeader("Content-Type", "application/json");
       int httpCode = http.POST(json);   //Send the request
       String payload = http.getString();                                        //Get the response payload
@@ -215,12 +228,26 @@ void cardIDfromUno(String cardID) {
     delay(10000);
   }
 }
+//void updateSerial()
+//{
+//  delay(500);
+//  while (Serial.available())
+//  {
+//    sim800L.write(Serial.read());//Forward what Serial received to Software Serial Port
+//  }
+//  while (sim800L.available())
+//  {
+//    Serial.write(sim800L.read());//Forward what Software Serial received to Serial Port
+//  }
+//}
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  while (sim800L.available()) {
-    Serial.println(sim800L.readString());
+  updateSerial();
+  while (Serial.available())  {
+    //String s = Serial.readString();
+    //Serial.println(s);
+    sim800L.println(Serial.readString());
   }
   while (uno.available() > 0) {
     StaticJsonDocument<1000> doc;
@@ -265,18 +292,49 @@ void loop() {
   GetData(content.substring(1));
   Serial.println();
   delay(5000);
+  String val = "Hello";
+  StaticJsonDocument<1000> doc;
+  doc["cardID"] = val;
+  //Send data to NodeMCU
+  serializeJson(doc, uno);
+  delay(500);
 
 }
-void send_sms(String Number)
+void send_sms(String num)
 {
-    Serial.println("sending sms....");
-    delay(50);
-    sim800L.print("AT+CMGF=1\r");
-    delay(1000);
-    sim800L.print("AT+CMGS=\""+Number+"\"\r");
-    delay(1000);
-    sim800L.print("Your Bag is in CheckIn 2");
-    delay(100);
-    sim800L.write(0x1A); //ascii code for ctrl-26 //Serial2.println((char)26); //ascii code for ctrl-26
-    delay(5000);
+  Serial.println("sending sms....");
+  delay(50);
+  sim800L.print("AT+CMGF=1\r");
+  delay(1000);
+  sim800L.print("AT+CMGS=\"" + num + "\"\r");
+  delay(1000);
+  sim800L.print("Your Bag is in CheckIn 1");
+  delay(100);
+  sim800L.write(0x1A); //ascii code for ctrl-26 //Serial2.println((char)26); //ascii code for ctrl-26
+  delay(5000);
+}
+void send_sms1(String num)
+{
+  Serial.println("sending sms....");
+  delay(50);
+  sim800L.print("AT+CMGF=1\r");
+  delay(1000);
+  sim800L.print("AT+CMGS=\"" + num + "\"\r");
+  delay(1000);
+  sim800L.print("Your Bag is in CheckIn 2");
+  delay(100);
+  sim800L.write(0x1A); //ascii code for ctrl-26 //Serial2.println((char)26); //ascii code for ctrl-26
+  delay(5000);
+}
+void updateSerial()
+{
+  delay(500);
+  while (Serial.available())
+  {
+    sim800L.write(Serial.read());//Forward what Serial received to Software Serial Port
+  }
+  while (sim800L.available())
+  {
+    Serial.write(sim800L.read());//Forward what Software Serial received to Serial Port
+  }
 }
